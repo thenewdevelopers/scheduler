ca1b5802624fe05b401e56ba3e08aed0493983d4
James Smith <computer.smither@gmail.com>
2016-04-07 01:01:09 -0400

    added all shift

--- main.cpp
+++ main.cpp
@@ -14,10 +14,11 @@ class EmployeeClass{
 		int maxweekhours;
 		int minweekhours;
 		int currentworkedhoursfortheweek;
+		bool alreadyworking;
 		
 	public: //access specifier
 		//classObjectConstructor with parameters
-		EmployeeClass(string firstNameHere, string positionHere, float hourlyWageHere, int maxDayHoursHere, int maxWeekHoursHere, int workedHoursHere){
+		EmployeeClass(string firstNameHere, string positionHere, float hourlyWageHere, int maxDayHoursHere, int maxWeekHoursHere, int workedHoursHere, bool alreadyWorkingHere){
 			firstname = firstNameHere;
 			position = positionHere;
 			hourlywage = hourlyWageHere;
@@ -25,6 +26,7 @@ class EmployeeClass{
 			maxweekhours = maxWeekHoursHere;
 			currentworkedhoursfortheweek = workedHoursHere;
 			bool offdays[7] = {false, true, true, true, true, true, false};
+			alreadyworking = alreadyWorkingHere;
 		}	
 		
 		//classObjectConstructor without parameters
@@ -54,32 +56,34 @@ class EmployeeClass{
 		int WorkedHours(){
 			return currentworkedhoursfortheweek;
 		}
+		bool AlreadyWorkingCheck(){
+				return alreadyworking;
+		}
+		
+		bool AlreadyWorking(bool x){
+			alreadyworking = x;
+		}
 };
-
-//print function like in java
-void print(string text){
-	cout<<text;
-}
-//print on one line function like in java
-void println(string text){
-	cout<<text<<endl;
-}
 
 //Main program
 int main(){ 
 	
-	EmployeeClass person0("James", "backend", 9.00, 8, 40, 0);//these are just temporary. we will use files created by the users to input the employees
-	EmployeeClass person1("Jennie", "frontend", 8.00, 8, 28, 0);//
-	EmployeeClass person2("William", "backend", 9.00, 8, 28, 0);//
-	EmployeeClass person3("Dakota", "frontend", 8.00, 8, 28, 0);//
-	EmployeeClass person4("Pam", "frontend", 12.00, 8, 40, 0);//
-	EmployeeClass person5("Katie", "manager", 10.00, 8, 28, 0);//
-	EmployeeClass person6("Michelle", "manager", 14.00, 8, 48, 0);//
-	EmployeeClass person7("Janet", "backend", 10.00, 8, 40, 0);//
-	EmployeeClass person8("Justin", "manager", 15.00, 8, 48, 0);//
-	EmployeeClass person9("Andy", "manager", 25.00, 8, 70, 0);//
+	EmployeeClass person0("James", "backend", 9.00, 8, 40, 0, false);//these are just temporary. we will use files created by the users to input the employees
+	EmployeeClass person1("Jennie", "frontend", 8.00, 8, 28, 0, false);//
+	EmployeeClass person2("William", "backend", 9.00, 8, 28, 0, false);//
+	EmployeeClass person3("Dakota", "frontend", 8.00, 8, 28, 0, false);//
+	EmployeeClass person4("Pam", "frontend", 12.00, 8, 40, 0, false);//
+	EmployeeClass person5("Katie", "manager", 10.00, 8, 28, 0, false);//
+	EmployeeClass person6("Michelle", "manager", 14.00, 8, 48, 0, false);//
+	EmployeeClass person7("Janet", "backend", 10.00, 8, 40, 0, false);//
+	EmployeeClass person8("Justin", "manager", 15.00, 8, 48, 0, false);//
+	EmployeeClass person9("Andy", "manager", 25.00, 8, 70, 0, false);//
+	EmployeeClass person10("Thomas", "backend", 10.00, 8, 40, 0, false);//
+	EmployeeClass person11("Shawme", "frontend", 8.00, 8, 28, 0, false);//
+	EmployeeClass person12("A.J.", "backend", 9.00, 8, 28, 0, false);//
+	EmployeeClass person13("Garret", "backend", 9.00, 8, 28, 0, false);//
 	
-	EmployeeClass employeelist[10] = {person0, person1, person2, person3, person4, person5, person6, person7, person8, person9 };
+	EmployeeClass employeelist[14] = {person0, person1, person2, person3, person4, person5, person6, person7, person8, person9, person10, person11, person12, person13};
 	
 	srand(time(0));
 	int shift = 0;	// "0" is for opening manager, "1" is for opening backend, "2" is for opening cashier, "3" is for mid backend #1, "4" is for mid backend #2, "5" is for closing manager, "6" is for closing backend, "7" is for closing cashier 
@@ -95,49 +99,105 @@ int main(){
 		//This is the first shift loop that adds people for that shift
 		while(shift <=2){
 			//random index of employeelist array and a variable/pointer that just tells the computer which person is being used. it isnt a copy like a normal variable
-			EmployeeClass *exactemployeeobject = &employeelist[rand()%10];
+			EmployeeClass *exactemployeeobject = &employeelist[rand()%14];
 			
 			switch(shift){
 				
 				//opening shift assignment
 /*###*/		case 0: 
 				
-				//adds manager to the "Day" array at index of 0
-				if(managersworking != 1 && exactemployeeobject->Position() == "manager"){
-					Day[0] = exactemployeeobject->Name();
-					managersworking++;
-					cout<<exactemployeeobject->Name()<<endl;
-				}
-				
-				//adds backend team member to the "Day" array at index of 1 
-				else if(backendworking != 1 && exactemployeeobject->Position() == "backend"){
-					Day[1] = exactemployeeobject->Name();
-					backendworking++;
-					cout<<exactemployeeobject->Name()<<endl;
-				}
-				
-				//adds cashier to the "Day" array at index 2
-				else if(cashiersworking != 1 && exactemployeeobject->Position() == "frontend"){
-					Day[2] = exactemployeeobject->Name();
-					cashiersworking++;
-					cout<<exactemployeeobject->Name()<<endl;
-				}
-				
-				// ends opening shift assignment
-				if(cashiersworking == 1 && managersworking == 1 && backendworking == 1){
-					cout<<"finished";
-					int managersworking = 0;
-					int cashiersworking = 0;
-					int backendworking = 0;
-					shift++;
-				}
-				
-				break;
+					//adds manager to the "Day" array at index of 0
+					if(managersworking < 1 && exactemployeeobject->Position() == "manager" && exactemployeeobject->AlreadyWorkingCheck() != true){
+						Day[0] = exactemployeeobject->Name();
+						exactemployeeobject->AlreadyWorking(true);
+						managersworking++;
+						cout<<exactemployeeobject->Name() + " " + "7-3" <<endl;
+					}
+					
+					//adds backend team member to the "Day" array at index of 1 
+					else if(backendworking < 1 && exactemployeeobject->Position() == "backend" && exactemployeeobject->AlreadyWorkingCheck() != true){
+						Day[1] = exactemployeeobject->Name();
+						exactemployeeobject->AlreadyWorking(true);
+						backendworking++;
+						cout<<exactemployeeobject->Name() + " " + "7-3" <<endl;
+					}
+					
+					//adds cashier to the "Day" array at index 2
+					else if(cashiersworking < 1 && exactemployeeobject->Position() == "frontend" && exactemployeeobject->AlreadyWorkingCheck() != true){
+						Day[2] = exactemployeeobject->Name();
+						exactemployeeobject->AlreadyWorking(true);
+						cashiersworking++;
+						cout<<exactemployeeobject->Name() + " " + "7-3" <<endl;
+					}
+					
+					// ends opening shift assignment
+					if(cashiersworking == 1 && managersworking == 1 && backendworking == 1){
+						shift++;
+						managersworking = 0;
+						cashiersworking = 0;
+						backendworking = 0;
+					}
+					
+					break;
 				
 /*###*/		case 1:
+
+					if(backendworking == 0 && exactemployeeobject->Position() == "backend" && exactemployeeobject->AlreadyWorkingCheck() != true){
+						Day[3] = exactemployeeobject->Name();
+						exactemployeeobject->AlreadyWorking(true);
+						cout<<exactemployeeobject->Name() + " " + "10-6" <<endl;
+						backendworking++;
+
+					}
+					
+					else 	if(backendworking == 1 && exactemployeeobject->Position() == "backend" && exactemployeeobject->AlreadyWorkingCheck() != true){
+						Day[4] = exactemployeeobject->Name();
+						exactemployeeobject->AlreadyWorking(true);
+						cout<<exactemployeeobject->Name() + " " + "10-6" <<endl;
+						backendworking++;
+					}
+					
+					if(backendworking == 2){
+						shift++;
+						managersworking = 0;
+						cashiersworking = 0;
+						backendworking = 0;
+					}
+					
 					break;
 					
 /*###*/		case 2:
+					
+					if(managersworking < 1 && exactemployeeobject->Position() == "manager" && exactemployeeobject->AlreadyWorkingCheck() != true){
+						Day[5] = exactemployeeobject->Name();
+						exactemployeeobject->AlreadyWorking(true);
+						cout<<exactemployeeobject->Name() + " " + "1-9" <<endl;
+						managersworking++;
+					}
+					
+					//adds backend team member to the "Day" array at index of 1 
+					else if(backendworking < 1 && exactemployeeobject->Position() == "backend" && exactemployeeobject->AlreadyWorkingCheck() != true){
+						Day[6] = exactemployeeobject->Name();
+						exactemployeeobject->AlreadyWorking(true);
+						cout<<exactemployeeobject->Name() + " " + "1-9" <<endl;
+						backendworking++;
+					}
+					
+					//adds cashier to the "Day" array at index 2
+					else if(cashiersworking < 1 && exactemployeeobject->Position() == "frontend" && exactemployeeobject->AlreadyWorkingCheck() != true){
+						Day[7] = exactemployeeobject->Name();
+						exactemployeeobject->AlreadyWorking(true);
+						cout<<exactemployeeobject->Name() + " " + "1-9" <<endl;
+						cashiersworking++;
+					}
+					
+					// ends opening shift assignment
+					if(cashiersworking == 1 && managersworking == 1 && backendworking == 1){
+						shift++;
+						managersworking = 0;
+						cashiersworking = 0;
+						backendworking = 0;
+					}
 					break;
 				
 			}
